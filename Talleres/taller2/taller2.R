eb70928d10047709e77a3002d65c8531a08bbe80
pablopulido2 <pablo.pulido@javeriana.edu.co>
2019-02-27 23:11:09 -0500

    Update and rename taller2.r to taller2.R

--- Talleres/taller2/taller2.R
+++ Talleres/taller2/taller2.R
@@ -223,5 +223,39 @@ print(Ab)
 holi = Q %*% R
 print (holi)
 
+#PUNTO 7
+#7A
+punto7 = function(x) {
+  n = length(x)
+  F = rep(NA, n)
+  F[1] = x[1] - x[2]
+  F[2] = x[1]^2 + x[2]^2 -1
+  F
+}
+p0 = c(1,1) 
+sol = BBsolve(par=p0, fn=punto7)
+sol$par
+
+plot(sol$par)
+plot(ecuaciones)
+#7B
+trigexp = function(x) {
+n = length(x)
+F = rep(NA, n)
+F[1] = 3*x[1]^2 + 2*x[2] - 5 + sin(x[1] - x[2]) * sin(x[1] + x[2])
+tn1 = 2:(n-1)
+F[tn1] = -x[tn1-1] * exp(x[tn1-1] - x[tn1]) + x[tn1] *
+( 4 + 3*x[tn1]^2) + 2 * x[tn1 + 1] + sin(x[tn1] -
+x[tn1 + 1]) * sin(x[tn1] + x[tn1 + 1]) - 8
+F[n] = -x[n-1] * exp(x[n-1] - x[n]) + 4*x[n] - 3
+F
+}
+n = 10000
+p0 = runif(n) # n initial random starting guesses
+sol = BBsolve(par=p0, fn=trigexp)
+sol$par
+
+#8
+
 
 
